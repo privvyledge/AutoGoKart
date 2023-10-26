566a5823729e25f1b5a616d3057268a6d7a5a439
ZYblend <zyby170412@gmail.com>
2023-10-26 17:58:41 -0400

    modify setup

--- AT9S_joy/setup.py
+++ AT9S_joy/setup.py
@@ -5,7 +5,7 @@ package_name = 'AT9S_joy'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -23,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+        'joy = AT9S_joy.joy_receiver:main'
         ],
     },
 )
