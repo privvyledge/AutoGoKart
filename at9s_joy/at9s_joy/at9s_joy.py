29aa0c5592cbe868605dae2503f4321bca752fab
ZYblend <zyblend@PC-D-BBC19F2F92>
2023-10-26 19:05:38 -0400

    solve the package not identified issue

--- at9s_joy/at9s_joy/at9s_joy.py
+++ at9s_joy/at9s_joy/at9s_joy.py
@@ -6,29 +6,25 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
-
-class JoyPublisher(Node):
-
-    def __init__(self):
-        super().__init__('joy_publisher')
-        self.steering_publisher_ = self.create_publisher(String, 'steering', 1)
-        self.throttle_publisher_ = self.create_publisher(String, 'throttle', 1)
-        timer_period = 0.05  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-
-        
-        self.ser = serial.Serial( port = '/dev/ttyACM0',baudrate=115200)  # series port of arduino
+class ATJoyNode(Node):
+	def __init__(self):
+		super().__init__('atjoy_node')
+		self.steering_publisher_ = self.create_publisher(String, 'steering', 1)
+		self.throttle_publisher_ = self.create_publisher(String, 'throttle', 1)
+		timer_period = 0.05  # seconds
+		self.timer = self.create_timer(timer_period, self.timer_callback)
+		
+		self.ser = serial.Serial( port = '/dev/ttyACM0',baudrate=115200)  # series port of arduino
 		self.ser.isOpen()  # open port receiver
-
-    def timer_callback(self):
+		
+	def timer_callback(self):
 		out = ''
 		while ser.inWaiting() > 0:
 			out += str(ser.readline(),encoding='utf-8')
-		
-		
+			
 		if out != '':
 			spl = out.split(",")
-					
+			
 			# Publisher
 			msg_thro  = String()
 			msg_steer = String()
@@ -43,13 +39,12 @@ class JoyPublisher(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    joy_publisher = JoyPublisher()
+    atjoy_node = ATJoyNode()
 
-    rclpy.spin(joy_publisher)
+    rclpy.spin(atjoy_node)
 
-    joy_publisher.destroy_node()
+    atjoy_node.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
--- at9s_joy/setup.py
+++ at9s_joy/setup.py
@@ -1,29 +1,26 @@
 from setuptools import find_packages, setup
 
-package_name = 'AT9S_joy'
+package_name = 'at9s_joy'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools',
-                      'pyserial',
-                      'numpy',
-                      'pyyaml'],
+    install_requires=['setuptools'],
     zip_safe=True,
     maintainer='zyblend',
-    maintainer_email='zyby170412@gmail.com',
+    maintainer_email='zyblend@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'joy = AT9S_joy.joy_receiver:main'
+        'at9s_joy = at9s_joy.at9s_joy:main'
         ],
     },
 )
