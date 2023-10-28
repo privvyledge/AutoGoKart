5336161c8126d84974cfd13a824b1698eb1a8dac
ZYblend <zyby170412@gmail.com>
2023-10-27 20:02:04 -0400

    SBW-test done

--- command_center/command_center/command_center_node.py
+++ command_center/command_center/command_center_node.py
@@ -8,21 +8,21 @@ class Command_Node(Node):
 	def __init__(self):
 		super().__init__("gokart_command")
 		self.get_logger().info("Node Created")
-		self.subscribe = self.create_subscription(Joy,"/control_command",self.getControll,10)
+		self.subscribe = self.create_subscription(PointStamped,"/control_command",self.getControll,10)
 		self.sbw_publisher = self.create_publisher(PointStamped,"/sbw_control",10)
 		
 	def map(self,input):
 		value = float(input +1) / 2
-		return -30 + (value*60)
+		return -90 + (value*180)
 		
 	def getControll(self,msg):
 		data = msg
 		msg = PointStamped();
 		msg.header.frame_id = "sbw_input"
-		msg.point.x = self.map(data.point.x)
-		msg.point.y = self.rl
+		msg.point.x = self.map(data.point.y)
+		msg.point.y = 0.0
 		msg.point.z = 0.0
-		self.publisher.publish(msg)
+		self.sbw_publisher.publish(msg)
 	
 def main(args=None):
 	rclpy.init(args=args)
