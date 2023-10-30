6747bc7a0d0cbd1ba4c2ef8d7737a376eab8da81
ZYblend <zyblend@PC-D-BBC19F2F92>
2023-10-30 18:21:58 -0400

    add joystick buttons

--- at9s_joy/at9s_joy/at9s_joy.py
+++ at9s_joy/at9s_joy/at9s_joy.py
@@ -5,14 +5,14 @@ import rclpy
 from rclpy.node import Node
 
 # from std_msgs.msg import Float64
-from geometry_msgs.msg import PointStamped
+from sensor_msgs.msg import Joy
 
 class ATJoyNode(Node):
 	def __init__(self):
 		super().__init__('atjoy_node')
 		# self.steering_publisher_ = self.create_publisher(Float64, 'steering', 1)
 		# self.throttle_publisher_ = self.create_publisher(Float64, 'throttle', 1)
-		self.command_publisher_ = self.create_publisher(PointStamped, 'control_command', 1)
+		self.command_publisher_ = self.create_publisher(Joy, 'joy/at9s', 1)
 		timer_period = 0.05  # seconds
 		self.timer = self.create_timer(timer_period, self.timer_callback)
 		
@@ -37,21 +37,28 @@ class ATJoyNode(Node):
 			
 			# at beginning, arduino send some cache string data
 			if self.isint(spl):
+				# The first two are for axes
 				# mapping from [2000,1000] to [-1,1]
-				spl[0] = 1-2*(float(spl[0])-1000)/(2000-1000)	
-				spl[1] = 1-2*(float(spl[1])-1000)/(2000-1000)	
+				spl[0] = 1.0-2.0*(float(spl[0])-1000.0)/(2000.0-1000.0)	
+				spl[1] = 1.0-2.0*(float(spl[1])-1000.0)/(2000.0-1000.0)
+				for i in range(2,len(spl)):
+					# For buttons: back: 2000, forward: 1000;
+					if int(spl[i])<1500:
+						spl[i]=1;
+					else:
+						spl[i]=0;
 			
 				# Publisher
-				msg  = PointStamped()
+				msg  = Joy()
 				msg.header.stamp = self.get_clock().now().to_msg()
 				msg.header.frame_id = 'at9s_joy'
-				msg.point.x = float(spl[0])
-				msg.point.y = float(spl[1])
-				msg.point.z = 0.0  # for future use
+				msg.axes = spl[0:2]
+				msg.buttons = spl[2:len(spl)]
 				self.command_publisher_.publish(msg)
 				
 				# printing for debug purpose
-				self.get_logger().info('throttle: "%f, steering: "%f"' % (msg.point.x, msg.point.y))
+				self.get_logger().info('throttle: "%f, steering: "%f" /n' % (msg.axes[0], msg.axes[1]))
+				self.get_logger().info('sWA: "%d, sWD: "%d" /n' % (msg.buttons[0],msg.buttons[3]))
 
 
 def main(args=None):
